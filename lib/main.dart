2a68992edc1882ab5eb4c9ed3537419c9c0889c9
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-03-10 22:52:08 +0530

    update frame and set up

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,26 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(height: 80,),
-            Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),)
-          ,]
+            Padding(padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                Text("Login", style: TextStyle(color: Colors.white, fontSize: 40),),
+                SizedBox(height: 10,),
+                Text("    Welcome Back", style: TextStyle(color: Colors.white, fontSize: 18),),
+              ],
+            ),
+            ),
+            SizedBox(height: 15,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(60),topRight: Radius.circular(60))
+
+                ),
+              ), 
+            )  
+          ]
         ),
       ),
     );
